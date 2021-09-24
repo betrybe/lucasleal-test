d1b66ba6b5c9e30576c195c0b88c8188c28bc408
Lucas Leal <lealzitooo@gmail.com>
2021-09-24 01:34:53 -0300

    Move funções para os objetos

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 let carrinhoElem;
 
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
+
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
+
 const storage = {
   add: (dados) => {
     const ls = JSON.parse(localStorage.getItem('carrinho') || '[]');
@@ -29,36 +43,31 @@ const product = {
     method: 'GET',
     redirect: 'follow',
   }),
+  getSkuFromProductItem: (item) => item.querySelector('span.item__sku').innerText,
 };
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  const index = [].indexOf.call(event.target.parentElement.children, event.target);
-  storage.remove(index);
-  event.target.parentNode.removeChild(event.target);
-  const ls = JSON.parse(localStorage.getItem('carrinho') || '[]');
-  const total = ls.reduce((sum, obj) => obj.salePrice + sum, 0);
-  document.querySelector('.total-price')
-  .textContent = total;
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
 const cart = {
+  itemClickListener: (event) => {
+    const index = [].indexOf.call(event.target.parentElement.children, event.target);
+    storage.remove(index);
+    event.target.parentNode.removeChild(event.target);
+    const ls = JSON.parse(localStorage.getItem('carrinho') || '[]');
+    const total = ls.reduce((sum, obj) => obj.salePrice + sum, 0);
+    document.querySelector('.total-price')
+    .textContent = total;
+  },
+  createElement: ({ sku, name, salePrice }) => {
+    const li = document.createElement('li');
+    li.className = 'cart__item';
+    li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+    li.addEventListener('click', cart.itemClickListener);
+    return li;
+  },
   add: (e) => {
     if (e.target.classList.contains('loading')) return;
     e.target.classList.add('loading');
     e.target.textContent = 'loading...';
-    product.get(getSkuFromProductItem(e.target.closest('.item')))
+    product.get(product.getSkuFromProductItem(e.target.closest('.item')))
     .then((response) => response.json())
     .then((produto) => {
       const dados = { sku: produto.id, name: produto.title, salePrice: produto.price };
@@ -71,7 +80,7 @@ const cart = {
     })
     .catch((error) => console.log('error', error));
   },
-  addElement: (dados) => carrinhoElem.appendChild(createCartItemElement(dados)),
+  addElement: (dados) => carrinhoElem.appendChild(cart.createElement(dados)),
   calc: () => {
     const ls = JSON.parse(localStorage.getItem('carrinho') || '[]');
     const total = ls.reduce((sum, obj) => obj.salePrice + sum, 0);
@@ -87,21 +96,7 @@ const cart = {
   },
 };
 
-function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
-}
-
-function createCustomElement(element, className, innerText) {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
-  return e;
-}
-
-function createProductItemElement({ sku, name, image }) {
+product.createElement = ({ sku, name, image }) => {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -113,7 +108,7 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(btn);
 
   return section;
-}
+};
 
 window.onload = () => {
   carrinhoElem = document.querySelector('ol.cart__items');
@@ -125,7 +120,7 @@ window.onload = () => {
     p.forEach((el) => {
       document.querySelector('section.items')
       .appendChild(
-        createProductItemElement({ sku: el.id, name: el.title, image: el.thumbnail }),
+        product.createElement({ sku: el.id, name: el.title, image: el.thumbnail }),
       );
     });
   }).catch((error) => console.log('error', error));
